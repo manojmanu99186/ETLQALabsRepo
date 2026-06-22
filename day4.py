b1eb0bd60e2d9815215b8c841534b82b2c29e504
manojmanu99186 <anmanoj09@gmail.com>
2026-06-22 12:03:41 +0530

    first commit

---
+++ day4.py
@@ -0,0 +1,2 @@
+s='bangalorecity'
+print(s[0:4:1])
---
+++ factorial.py
@@ -0,0 +1,20 @@
+def factorial_for():
+    n=5
+    f=1
+    for i in range(1, n+1):
+        f=f*i
+    print(f)
+factorial_for()
+
+def facotrial_while():
+    n=4
+    f=1
+    while n>1:
+        f=f*n
+        n=n-1
+    print(f)
+facotrial_while()
+
+
+
+
